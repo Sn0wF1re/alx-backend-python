665ec094bb167f190a45e55c7e551530c5c33bfa
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-27 16:07:21 +0300

    Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Create a function add to return sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a - float
+        b - float
+    Return: sum
+    """
+    return a + b
