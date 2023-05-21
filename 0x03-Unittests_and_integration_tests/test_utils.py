76a64db54c6d1d72354c83255e5813e6d15537d0
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-21 17:53:50 +0300

    Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Create a TestAccessNestedMap class
+that inherits from unittest.TestCase
+"""
+import unittest
+from utils import access_nested_map
+from typing import Mapping, Sequence
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Write unit tests
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
+        """
+        test that the method returns what it is supposed to
+        """
+        resp = access_nested_map(nested_map, path)
+        self.assertEqual(resp, expected)
