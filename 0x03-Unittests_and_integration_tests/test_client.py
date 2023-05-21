4619e43a376a271ecdba6c28e1d7a3a69e2ae97c
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-21 22:46:30 +0300

    Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Write unit tests for client.py
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 import client
 from client import GithubOrgClient
@@ -26,3 +26,16 @@ class TestGithubOrgClient(unittest.TestCase):
         resp = test.org
         self.assertEqual(resp, mock.return_value)
         mock.assert_called_once()
+
+    def test_public_repos_url(self):
+        """
+        unit-test GithubOrgClient._public_repos_url
+        """
+        with patch.object(GithubOrgClient, "org",
+                          new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": 89}
+            testOrg = GithubOrgClient('holberton')
+            test_result = testOrg._public_repos_url
+            self.assertEqual(test_result,
+                             mock_org.return_value.get("repos_url"))
+            mock_org.assert_called_once()
